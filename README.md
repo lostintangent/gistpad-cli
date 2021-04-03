67c84f57b057431337ce8a65d9c417755d4fbf22
Jonathan Carter <jc@trl.mx>
2021-04-03 21:44:03 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# gistpad-cli
-A companion CLI that makes it easy to open gists and repos in VS Code, directly from your terminal
+# GistPad CLI 📘
+
+GistPad CLI is a companion CLI for the [GistPad extension](https://aka.ms/gistpad), that makes it easy to open gists and repos in VS Code, directly from your terminal.
+
+## Getting Started
+
+1. Run `npm i -g gistpad-cli`
+1. Run `gistpad <gistOrRepo>` (e.g. `gistpad lostintangent/gistpad`, `gistpad 9fe2a7792b025db004ff44b91fccdfbf`)
+
+    > Alternatively, you could just run `npx gistpad-cli <gistOrRepo>` if you'd prefer not to install the CLI.
+
+This will launch VS Code, and open the specified repo or gist. If you don't already have the GistPad extension installed, it will walk you through installing it. 
+
+## Launch Behavior
+
+If you specify a gist ID, then GistPad will open the files contained within the gist. However, if you specify a repo name, then GistPad will open the repo's `README` (if it has one), and then automatically focus the specified repo in the `Repos` tree view. This makes it easy to browse/edit other files within the repo.
+
+## VS Code Insiders
+
+If you'd prefer to open the specified gist or repo within VS Code Insiders, then simply pass the `-i` or `--insiders` flag to the `gistpad` command. For example: `gistpad -i lostintangent/gistpad`.
---
+++ index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const insidersFlag = ["--insiders", "-i"];
+const usageInfo = `gistpad [${insidersFlag.join("|")}] <gistOrRepo>.`;
+
+function printHelp() {
+    console.log(
+        `👋 Specify the name of a GitHub repo (owner/repo) or gist ID: ${usageInfo}`
+    );
+}
+
+let [,, flag, gistOrRepo] = process.argv;
+
+// If the user just ran "gistpad", then we
+// want to display the CLI help text w/args.
+if (!flag && !gistOrRepo) {
+    return printHelp();
+}
+
+if (!gistOrRepo) {
+    // If the user just ran "gistpad -i",
+    // then print the help text.
+    if (flag.startsWith("-")) {
+        return printHelp();
+    } else {
+        // The user didn't specify a build flag, so
+        // we'll treat the first arg as the repo/gist.
+        gistOrRepo = flag;
+        flag = undefined;
+    }
+} else {
+    // The user specified two flags, so let's verify that
+    // they provided a valid VS Code quality type or not.
+    if (!insidersFlag.includes(flag)) {
+        return console.error(
+            `❌ The specified VS Code quality isn't valid. Please try again: ${usageInfo}`
+        );
+    }
+}
+
+const quality = insidersFlag.includes(flag) ? "vscode-insiders" : "vscode";
+const param = gistOrRepo.includes("/") ? "repo" : "gist"
+
+const url = `${quality}://vsls-contrib.gistfs/open?${param}=${gistOrRepo}`;
+require("open")(url);
